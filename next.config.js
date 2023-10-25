152e4fa6e3f59d08cbd9ef68eea4b36b922d8267
Guille <guillermofcampoamor@gmail.com>
2023-10-26 00:36:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
     output: "export",
     basePath: "/nextjs-posts",
-    experimental: {
-        appDir: true
-    },
     images: {
-        domains: ['robohash.org', 'img.dog-learn.com'],
         unoptimized: true
     },
 }
