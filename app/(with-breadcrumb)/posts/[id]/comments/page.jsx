94cc0af34be1b982fdd74a36a7e05bf16f9b71b4
Guille <guillermofcampoamor@gmail.com>
2023-10-26 00:46:09 +0200

    Update page.jsx

--- app/(with-breadcrumb)/posts/[id]/comments/page.jsx
+++ app/(with-breadcrumb)/posts/[id]/comments/page.jsx
@@ -1,9 +1,11 @@
 
 import ListOfComments from "../../../../components/ListOfComments/ListOfComments"
 
-export default function CommentsPage({params}) {
+export default async function CommentsPage({params}) {
   let { id } = params
   return (
-    <ListOfComments id={id}/>
+    <>{
+      <ListOfComments id={id}/>
+    }</>
   )
 }
